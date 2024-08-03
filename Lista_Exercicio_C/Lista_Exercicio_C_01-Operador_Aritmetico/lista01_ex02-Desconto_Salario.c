dc431e8684b4828c81cb1a3725b106466ebf104d
Thales <thaleslima225@gmail.com>
2024-08-03 01:03:23 -0300

    ex02 first

--- Lista_Exercicio_C/Lista_Exercicio_C_01-Operador_Aritmetico/lista01_ex02-Desconto_Salario.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_01-Operador_Aritmetico/lista01_ex02-Desconto_Salario.c
@@ -1,6 +1,6 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "04/08/2022"
+//  Data....: "03/08/2024"
 /*	Objetivo:
 Imagine a seguinte situação: Desconta-se inicialmente 10% do salário bruto do trabalhador como contribuição à previdência social.
 Após esse desconto, há outro desconto de 5% sobre o valor restante do salário bruto a título de um determinado imposto.
@@ -13,24 +13,21 @@ Faça um programa em C que leia o salário bruto de um cidadão e apresente o se
 int main(void){
 //Declarações
 	float salarioBruto;
+
 //Instruções
-	//printf("");
-	//scanf("%",&);
-	
 	printf("Digite seu salario bruto: ");
 	scanf("%f",&salarioBruto);
 	
-	printf("\nPrimeiro desconto: %.4f\n", (salarioBruto * 0.10));
+	printf("\nPrimeiro imposto de 10%%: %.4f\n", (salarioBruto * 0.10));
 		salarioBruto -= salarioBruto * 0.10;
-	printf("Salario com 1o Imposto: %.4f\n",salarioBruto);
-	printf("Segundo desconto: %.4f\n", (salarioBruto * 0.05));
+	printf("Salario descontado: %.4f\n",salarioBruto);
+	
+	printf("\nSegundo imposto de 5%%: %.4f\n", (salarioBruto * 0.05));
 		salarioBruto -= salarioBruto * 0.05;
-	printf("Salario com 2o Imposto: %.4f\n",salarioBruto);
-	//system("pause");
+	printf("Salario descontado: %.4f\n",salarioBruto);
+	
 	return 0;
 }
-
-
 
 // FIM *************************************************************************************************************************
 
