8127020f7b0541065aea751d7a061aade4da2941
Thales <thaleslima225@gmail.com>
2024-08-03 21:44:07 -0300

    ex10 ok

--- Lista_Exercicio_C/Lista_Exercicio_C_02-Condicional/lista02_ex10-Jogo_24h.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_02-Condicional/lista02_ex10-Jogo_24h.c
@@ -1,10 +1,10 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "05/08/2022"
+//  Data....: "03/08/2024"
 /*	Objetivo:
 Faça um programa em C que receba a hora de início e a hora de fim de um jogo de xadrez 
-(considere apenas horas inteiras, sem os minutos), calcule e imprima a duração do jogo em 
-horas. Sabe-se que o tempo máximo de duração do jogo é de 24 horas e que o jogo pode 
+(considere apenas horas inteiras, sem os minutos), calcule e imprima a duração do jogo em horas.
+Sabe-se que o tempo máximo de duração do jogo é de 24 horas e que o jogo pode 
 iniciar em um dia e terminar no dia seguinte.
 */
 #include<stdio.h>
@@ -17,31 +17,28 @@ int main(void){
 	int duracao;
 	
 //Instruções
-	do{
-		printf("\nQual a hora de inicio do jogo: ");
-		scanf("%d",&horaInicio);
-		
-		printf("\nQual a hora de fim do jogo: ");
-		scanf("%d",&horaFim);
-		
-		if(horaInicio > horaFim){
-			duracao = (24 - horaInicio) + horaFim;
-			printf("\n==================================\n");
-			printf("  Duracao de %dh durante 2 dias!",duracao);
-			printf("\n==================================\n");
-		}else if(horaInicio < horaFim){
-			duracao = (horaFim - horaInicio);
-			printf("\n==================================\n");
-			printf("  Duracao de %dh durante 1 dia!",duracao);
-			printf("\n==================================\n");
-		}else{
-			duracao = (horaFim + horaInicio);
-			printf("\n==================================\n");
-			printf("  Duracao de %dh durante 2 dias!",duracao);
-			printf("\n==================================\n");
-		}
-	}while(duracao>24);
+	printf("\nQual a hora de inicio do jogo: ");
+	scanf("%d",&horaInicio);
 	
+	printf("\nQual a hora de fim do jogo: ");
+	scanf("%d",&horaFim);
+	
+	if(horaInicio > horaFim){
+		duracao = (24 - horaInicio) + horaFim;
+		printf("\n==================================\n");
+		printf("  Duracao de %dh durante 2 dias!",duracao);
+		printf("\n==================================\n");
+	}else if(horaInicio < horaFim){
+		duracao = (horaFim - horaInicio);
+		printf("\n==================================\n");
+		printf("  Duracao de %dh durante 1 dia!",duracao);
+		printf("\n==================================\n");
+	}else{
+		duracao = (horaFim + horaInicio);
+		printf("\n==================================\n");
+		printf("  Duracao de %dh durante 2 dias!",duracao);
+		printf("\n==================================\n");
+	}	
 	return 0;
 }
 
