c35ef25b5d9e044ee47fda6c5882c09860d49f28
Thales <thaleslima225@gmail.com>
2024-08-08 21:10:52 -0300

    ex04 ok

--- Lista_Exercicio_C/Lista_Exercicio_C_03-Laco_de_Repeticao/lista03_ex04-Media_Populacao.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_03-Laco_de_Repeticao/lista03_ex04-Media_Populacao.c
@@ -1,6 +1,6 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "06/12/2021"
+//  Data....: "08/08/2024"
 /*	Objetivo:
 A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre 
 o salário e número de filhos. A prefeitura deseja saber:
@@ -15,13 +15,16 @@ O final da leitura de dados se dará com a entrada de um salário negativo.
 
 int main(void){
 //Declarações
-	float salario, maiorSalario;
-	float salarioAbaixo100, filhoTotal, salarioTotal;
-	int filho, qtd;
+	float salario=0, maiorSalario=0;
+	float salarioAbaixo100=0, filhoTotal=0, salarioTotal=0;
+	int filho=0, qtd=0;
 	
 //Instruções
-	printf("========================================================\n");
-	printf("Qual o sal�rio: ");
+	printf("=============================\n");
+	printf("   Pesquisa da prefeitura\n");
+	printf("=============================\n\n");
+
+	printf("Digite o salario: ");
 	scanf("%f",&salario);
 	
 	while(salario >= 0){
@@ -32,21 +35,30 @@ int main(void){
 		salarioTotal += salario;
 		filhoTotal += filho;
 		
-		if(salario > maiorSalario)
+		if(salario > maiorSalario){
 			maiorSalario = salario;
+		}
 			
-		if(salario<=100)
+		if(salario<=100){
 			salarioAbaixo100++;
+		}
 		
-		printf("\nDigite novo sal�rio ou -1 para finalizar!");
+		printf("\nDigite novo salario");
+		printf("\nou -1 para finalizar: ");
 		scanf("%f",&salario);
 	}
-	printf("\n========================================================");
-	printf("\nM�dia do sal�rio da popula��o: %.2f\n", salarioTotal/qtd);
-	printf("M�dia do n�mero de filhos: %.2f\n", filhoTotal/qtd);
-	printf("Maior sal�rio: %.2f\n", maiorSalario);
-	printf("Percentual de pessoas com sal�rio at� R$100,00: %.2f%%\n",(salarioAbaixo100*100)/qtd);
-		
+
+	if(qtd > 0){
+		printf("\n========================================================");
+		printf("\nMedia do salario da populacao: %.2f\n", salarioTotal/qtd);
+		printf("Media do numero de filhos: %.2f\n", filhoTotal/qtd);
+		printf("Maior salario: %.2f\n", maiorSalario);
+		printf("Percentual de pessoas com salario ate R$100,00: %.2f%%\n",(salarioAbaixo100*100)/qtd);
+		printf("========================================================\n\n");
+	}
+
+	printf("Valor negativo, programa encerrado!\n\n");
+	
 	return 0;
 }
 
