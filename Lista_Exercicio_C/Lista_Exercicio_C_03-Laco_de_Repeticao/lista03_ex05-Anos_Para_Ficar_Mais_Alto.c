786ac9e546656d4f85aeba45d439ea48c56ee6b0
Thales <thaleslima225@gmail.com>
2024-08-08 22:53:27 -0300

    ex05 ok usando %g

--- Lista_Exercicio_C/Lista_Exercicio_C_03-Laco_de_Repeticao/lista03_ex05-Anos_Para_Ficar_Mais_Alto.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_03-Laco_de_Repeticao/lista03_ex05-Anos_Para_Ficar_Mais_Alto.c
@@ -1,6 +1,6 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "06/12/2021"
+//  Data....: "08/08/2024"
 /*	Objetivo:
 Chico tem 1,50 metros e cresce 2 centímetros por ano, enquanto Zé tem 1,10 metro e cresce 
 3 centímetros por ano. Faça um programa em C que recebendo a altura e o crescimento 
@@ -13,22 +13,46 @@ mensagem de impossibilidade.
 
 int main(void){
 //Declarações
-	float chico = 1.5, ze = 1.1;
-	int anos;
-//Instruções
-	printf("Chico: %.2fm\n",chico);
-	printf("Z�...: %.2fm\n",ze);
+	float height1, height2, grow1, grow2;
+	int years;
+
+//Instruções	
+	//printf("Altura: %.2g",height1);
+
+	printf("Pessoa 1 Altura: ");
+	scanf("%f",&height1);
+	printf("Crescimento por ano: ");
+	scanf("%f",&grow1);
+	printf("\nPessoa 2 Altura: ");
+	scanf("%f",&height2);
+	printf("Crescimento por ano: ");
+	scanf("%f",&grow2);
+
+	if(height1 < height2){
+		do{
+			height1 += grow1;
+			height2 += grow2;
+			years ++;
+		}while(height1 <= height2 && years < 100);
+	}else if(height2 < height1){
+		do{
+			height1 += grow1;
+			height2 += grow2;
+			years ++;
+		}while(height2 <= height1 && years < 100);
+	}else{
+		printf("Alturas iguais!\n");
+	}
 	
-	do{
-		chico += 0.02;
-		ze += 0.03;
-		anos ++;
-	}while(ze<=chico);
-	
-	printf("\nChico: %.2fm\n",chico);
-	printf("Z�...: %.2fm\n",ze);
-	printf("Se passaram %d anos para Z� ficar maior que o Chico.", anos);
-	
+	if(height1 != height2){
+		if(years < 100){
+			printf("\nPessoa 1: %.9gm\n",height1);
+			printf("Pessoa 2: %.9gm\n",height2);
+			printf("\nSe passaram %d anos para a pessoa mais baixa ficar maior que a mais alta.\n", years);
+		}else{
+			printf("\nLimite de 100 anos. Medicao impossivel!\n");
+		}
+	}
 	return 0;
 }
 
