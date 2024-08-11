9aa4e1e6e1686d4db1611035f05d7d0443d5847a
Thales <thaleslima225@gmail.com>
2024-08-11 03:09:11 -0300

    ex07 ok

--- Lista_Exercicio_C/Lista_Exercicio_C_03-Laco_de_Repeticao/lista03_ex07-Media_Sem_Menor_Nota.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_03-Laco_de_Repeticao/lista03_ex07-Media_Sem_Menor_Nota.c
@@ -1,6 +1,6 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "06/12/2021"
+//  Data....: "11/08/2024"
 /*	Objetivo:
 Um professor deseja automatizar seu sistema de pontuação. Ele faz 21 provas por semestre 
 e então calcula a média usando apenas as 20 maiores notas. Uma média de 9.0 ou mais 
@@ -16,56 +16,52 @@ O programa deve encerrar quando for digitado "fim" para o nome.
 
 int main(void){
 //Declarações
-	char nome[10];
+	char nome[25];
 	float media, nota, notaMenor=10, notaTotal=0;
-	int i, flag=0;
-	//float media;
+	int i;
 	
 //Instruções
+	fflush(stdin); setbuf(stdin, 0);
+	printf("Digite -fim- para finalizar!\n");
 	printf("Nome do aluno: ");
-	//scanf("%s",nome);
-	gets(nome);
-	do{
+	scanf("%s",nome);
+
+	while(!(nome[0]=='f' && nome[1]=='i' && nome[2]=='m' && nome[3]=='\0')){
 		for(i=1; i<=ALUNOS; i++){
 			do{
-				printf("%d� Nota do %s: ",i,nome);
+				printf("%do Nota do %s: ",i,nome);
 				scanf("%f",&nota);
 			}while((nota<0) || (nota>10));
 			
-			if(nota<notaMenor)
+			if(nota < notaMenor)
 				notaMenor = nota;
 				
 			notaTotal += nota;
-			printf("%.0f\n\n",notaTotal);
 		}
+
 		printf("==========================\n");
-		printf("Total1: %.0f\n",notaTotal);
 		notaTotal -= notaMenor;
-		printf("Total2: %.0f\n",notaTotal);
-		media = notaTotal/(ALUNOS);
-		printf("M�dia: %.2f\n",media);
+		media = notaTotal/(ALUNOS-1);
+		printf("Media: %.2f\n",media);
 		
 		if(media >= 9)
-			printf("grau A\n");
+			printf("Grau A\n");
 		else if(media >= 8)
-			printf("grau B\n");
+			printf("Grau B\n");
 		else if(media >= 7)
-			printf("grau C\n");
+			printf("Grau C\n");
 		else if(media < 7)
-			printf("grau D\n");
+			printf("Grau D\n");
 		printf("==========================\n\n");
 		
 		notaTotal = 0;
 		notaMenor = 10;
 		
+		fflush(stdin); setbuf(stdin, 0);
 		printf("Digite -fim- para finalizar!\n");
 		printf("Nome do novo aluno: ");
-		scanf("%s",&nome);
-		
-		if (strcmp(nome, "fim")==0)
-			flag=1;
-			
-	}while(flag != 1);
+		scanf("%s",nome);
+	}
 	
 	return 0;
 }
