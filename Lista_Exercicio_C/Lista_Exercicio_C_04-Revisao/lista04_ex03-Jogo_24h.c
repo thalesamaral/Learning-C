8a1b60a3b302203a6c76daccb78c9c03e0017da9
Thales <thaleslima225@gmail.com>
2024-08-17 17:59:16 -0300

    done

--- Lista_Exercicio_C/Lista_Exercicio_C_04-Revisao/lista04_ex03-Jogo_24h.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_04-Revisao/lista04_ex03-Jogo_24h.c
@@ -1,6 +1,6 @@
 //  Síntese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "15/08/2024"
+//  Data....: "17/08/2024"
 /*	Objetivo:
 Escrever um programa em C que lê a hora de início e hora de término de um jogo, ambas 
 subdivididas em dois valores distintos: horas e minutos. Calcular e escrever a duração do 
@@ -9,63 +9,67 @@ jogo é de 24 horas e que o jogo pode iniciar em um dia e terminar no dia seguin
 */
 #include<stdio.h>
 #include<stdlib.h> //system();
-#define BARRA_DUPLA printf("\n==================================\n");
+#define BARRA_DUPLA printf("=============================================\n");
 
 int main(void){
 //Declarações
-	float horaInicio=9, minutoInicio=1;
-	float horaFim=9, minutoFim=0;
+	float horaInicio, minutoInicio;
+	float horaFim, minutoFim;
 	float duracao;
 	float totalInicio, totalFim;
 	
 //Instruções
 	
 	//INCIO: HORA *************
+	printf("Inicio do jogo\n");
 	do{
-		printf("Horario - Inicio do jogo\n");
 		printf(" Hora...: ");
 		scanf("%f",&horaInicio);
 		if(horaInicio < 0 || horaInicio > 23){
 			system("cls");
 			printf("Valor invalido!\n\n");
+			printf("Inicio do jogo\n");
 		}
 	}while(horaInicio < 0 || horaInicio > 23);
 	
 	//INCIO: MINUTO *************
 	do{
-		printf("Horario - Inicio do jogo\n");
 		printf(" Minuto.: ");
 		scanf("%f",&minutoInicio);
 		if(minutoInicio < 0 || minutoInicio > 59){
 			system("cls");
 			printf("Valor invalido!\n\n");
+			printf("Inicio do jogo\n");
 		}
 	}while(minutoInicio < 0 || minutoInicio > 59);
 
 	//FIM: HORA ******************
+	printf("\nFinal do jogo\n");
 	do{
-		printf("\nHorario - Final do jogo\n");
 		printf(" Hora...: ");
 		scanf("%f",&horaFim);
 		if(horaFim < 0 || horaFim > 23){
 			system("cls");
 			printf("Valor invalido!\n\n");
+			printf("Final do jogo\n");
 		}
 	}while(horaFim < 0 || horaFim > 23);
 
 	//FIM: MINUTO ******************
 	do{
-		printf("Horario - Final do jogo\n");
 		printf(" Minuto.: ");
 		scanf("%f",&minutoFim);
 		if(minutoFim < 0 || minutoFim > 59){
 			system("cls");
 			printf("Valor invalido!\n\n");
+			printf("Final do jogo\n");
 		}
 	}while(minutoFim < 0 || minutoFim > 59);
 	
-	printf("\nHorario - Inicio do jogo: %.0f:%.0f\n",horaInicio, minutoInicio);
-	printf("Horario - Final do jogo.: %.0f:%.0f\n",horaFim, minutoFim);
+	system("cls");
+	BARRA_DUPLA
+	printf(" Inicio do jogo.: %.0f Hora(s) e %.0f Minuto(s)\n",horaInicio, minutoInicio);
+	printf(" Final do jogo..: %.0f Hora(s) e %.0f Minuto(s)\n",horaFim, minutoFim);
 
 	totalInicio = horaInicio + minutoInicio/60;
 	totalFim = horaFim + minutoFim/60;
@@ -82,7 +86,7 @@ int main(void){
 	
 	int intpart = (int)duracao;
 	float decpart = duracao - intpart;
-	printf(" Duracao de %d:%.0fh",intpart,decpart*60);
+	printf(" Duracao de %d Hora(s) e %.0f Minuto(s)\n",intpart,decpart*60);
 	BARRA_DUPLA
 	return 0;
 }
