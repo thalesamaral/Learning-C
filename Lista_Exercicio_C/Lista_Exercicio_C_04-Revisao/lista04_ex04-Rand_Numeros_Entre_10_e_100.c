0d70eb599abca1a604686f50dbe4b7422bde02ba
Thales <thaleslima225@gmail.com>
2024-08-17 19:05:51 -0300

    ex done

---
+++ Lista_Exercicio_C/Lista_Exercicio_C_04-Revisao/lista04_ex04-Rand_Numeros_Entre_10_e_100.c
@@ -0,0 +1,36 @@
+// Síntese
+// Autor: Thales Amaral Lima
+// Data: 17/08/2024
+/* Objetivo:
+Crie um programa em C para ler 50 números e ao final informar quantos números estão no 
+intervalo entre 10 (inclusive) e 100 (inclusive).
+*/
+#include <stdio.h>
+#include<stdlib.h>// necessário p/ as funções rand() e srand()
+#include<time.h>// necessário p/ função time()
+#define TAM 50
+
+int main(void){
+//Declarações
+	int i, num, quant=0;
+
+//Instruções
+	//inicia o gerador de números pseudo-aleatórios usado pela função rand()
+	srand(time(NULL));
+
+	printf("Digite 50 numeros!\n\n");
+	for(i=1; i<=TAM; i++){
+		printf("Numero %d: ",i);
+		//scanf("%d",&num);
+		
+		/* gerando valores aleatórios entre 0 e 100 */
+		num = rand() % 100;
+		printf("%d\n",num);
+		
+		if(num>=10 && num <= 100){
+			quant ++;
+		}
+	}
+	printf("\nQuantidade de numeros entre 10 e 100: %d\n",quant);
+}
+
