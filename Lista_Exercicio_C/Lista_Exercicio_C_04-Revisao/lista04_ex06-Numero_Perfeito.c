1c205bacc992d03c92236d041a3388e49939591a
Thales <thaleslima225@gmail.com>
2024-08-18 00:25:35 -0300

    done

--- Lista_Exercicio_C/Lista_Exercicio_C_04-Revisao/lista04_ex06-Numero_Perfeito.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_04-Revisao/lista04_ex06-Numero_Perfeito.c
@@ -1,30 +1,47 @@
 // Síntese
 // Autor: Thales Amaral Lima
-// Data: 0/0/2024
-/* Objetivo: */
-#include <stdio.h>
+// Data: 17/08/2024
+/* Objetivo:
+Dado um número n inteiro e positivo, dizemos que n é perfeito se n for igual à soma de seus 
+divisores positivos diferentes de n. Construa um programa em C que verifica se um dado 
+número é perfeito. Ex: 6 é perfeito, pois 1+2+3 = 6.
+*/
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(void){
 //Declarações
-	int num,c,soma=0;
+	int i, num, soma=0;
 	
 //Instruções
     
     do{
-		printf("Digite um numero\n");
-		scanf("%i",&num);
-		if (num<1){
+		printf("---------------------------------------------\n");
+		printf(" N eh perfeito se N for igual a soma de seus\n");
+		printf(" divisores positivos diferentes de N\n");
+		printf("---------------------------------------------\n");
+		printf("Digite um numero: ");
+		scanf("%d",&num);
+		if(num < 2){
 			system("cls");
-			printf("Numero invalido, digite de 1 pra cima\n");
+			printf("Numero invalido, apenas acima de 1\n\n");
 		}
-	}while(num<1);
-    
-	for(c=1;c<num;c++)
-    	if(num % c == 0)
-        	soma = soma + c;
-	if(soma == num)
-		printf("Numero eh Perfeito");
-	else
-		printf("Numero eh Imperfeito");
+	}while(num < 2);
+    printf("\n");
+
+	for(i=1; i<num; i++){
+		if(num % i == 0){
+			printf(" + %d",i);
+			soma += i;
+		}
+	}
+
+	printf(" = %d\n",soma);
+
+	if(soma == num){
+		printf("Numero Perfeito -> %d\n\n",num);
+	}else{
+		printf("Numero Imperfeito -> %d\n\n",num);
+	}
 }
 
