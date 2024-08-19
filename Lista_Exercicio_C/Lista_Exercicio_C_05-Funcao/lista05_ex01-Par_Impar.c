8d3fa9ea502075816130fec6c46f5c8e700fdeca
Thales <thaleslima225@gmail.com>
2024-08-19 19:08:12 -0300

    done

--- Lista_Exercicio_C/Lista_Exercicio_C_05-Funcao/lista05_ex01-Par_Impar.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_05-Funcao/lista05_ex01-Par_Impar.c
@@ -1,15 +1,14 @@
 //  Síntese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "09/12/2021"
+//  Data....: "19/08/2024"
 /*	Objetivo:
 1. Faça um programa em C que contenha subprograma. É necessário que seu programa como um todo leia um número inteiro n, verifica e imprime se este é par ou ímpar.
 */
 #include<stdio.h>
-#include<stdlib.h>
 #define TAM
 
 //Protótipo de Função
-int verificarParImpar(int);
+void verificarParImpar(int);
 
 int main(void){
 //Declarações
@@ -17,24 +16,19 @@ int main(void){
 	
 //Instruções
 	
-	printf("Digite um n�mero: ");
+	printf("Digite um numero: ");
 	scanf("%d",&num);
 	
 	verificarParImpar(num);
 	
-	if(num % 2 == 0)
-		printf("PAR!\n");
-	else
-		printf("�MPAR!\n");
-	
 	return 0;
 }
 
-int verificarParImpar(int n){
+void verificarParImpar(int n){
 	
 	if(n % 2 == 0)
-		return n;
+		printf("\nPAR!\n");
 	else
-		return n;
+		printf("\nIMPAR!\n");
 	
 }
