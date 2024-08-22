ed535d4b0ac1f6e4a1e42965822383ed66dfcb4a
Thales <thaleslima225@gmail.com>
2024-08-22 01:56:11 -0300

    v1

--- Lista_Exercicio_C/Lista_Exercicio_C_05-Funcao/lista05_ex05-Produtos_20_Porcento.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_05-Funcao/lista05_ex05-Produtos_20_Porcento.c
@@ -1,55 +1,70 @@
 //  Síntese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "10/12/2021"
-/*	Objetivo: Seu programa deve ler o c�digo e o pre�o de custo de cada produto e calcular o novo pre�o.
-Calcule tamb�m, a m�dia dos pre�os com e sem aumento. Mostre o c�digo e o novo pre�o de cada produto e, 
-no final, as m�dias.*/
+//  Data....: "22/08/2024"
+/*	Objetivo:
+5. Faça um programa em C que contenha subprograma. É necessário que seu programa como 
+um todo trate uma empresa que deseja aumentar seus preços em 20%. Seu programa deve 
+ler o código e o preço de custo de cada produto e calcular o novo preço. Calcule também, a 
+média dos preços com e sem aumento. Mostre o código e o novo preço de cada produto e, 
+no final, as médias. A entrada de dados deve terminar quando for lido um código de produto 
+negativo.
+*/
 #include<stdio.h>
-#include<stdlib.h>
 
 //Protótipo de Função
-float calculaPreco20(int, float);
+float preco_20_porcento(float);
+int valida_numero();
 
+//*** BLOCO PRINCIPAL *****************************************************
 int main(void){
 //Declarações
 	int codigo, qtd;
-	float preco, precoInicial, preco20;
+	float preco, precoInicial=0, preco20=0;
 	
 //Instruções
-	
-	printf("Digite -1 para finalizar!\n");
-	printf("Digite o Codigo: ");
-	scanf("%d",&codigo);
+	codigo = valida_numero();
 	
 	while(codigo >= 0){
-		do{
-			printf("Digite o pre�o: ");
-			scanf("%f",&preco);
-			
-			qtd++;
-			precoInicial += preco;
-			
-			preco20 += calculaPreco20(codigo, preco);
-			
-			printf("\n==========================================\n");
-			printf("M�dia do pre�o inicial %.2f",precoInicial/qtd);
-			printf("\nM�dia do pre�o com 20%% %.2f",preco20/qtd);
-			printf("\n==========================================\n\n");
-			
-			printf("Digite -1 para finalizar!\n");
-			printf("Digite o Codigo: ");
-			scanf("%d",&codigo);
-		}while(codigo > 0);
+		printf("Digite o preco: ");
+		scanf("%f",&preco);
+		
+		qtd++;
+		precoInicial += preco;
+		
+		printf("\nCodigo: %d Preco antigo: %.2f",codigo, preco);
+		preco20 += preco_20_porcento(preco);
+		printf("\nCodigo: %d Preco novo..: %.2f\n",codigo,preco);
+		
+		printf("\n==========================================\n");
+		printf("Media do preco inicial: %.2f",precoInicial/qtd);
+		printf("\nMedia do preco com 20%%: %.2f",preco20/qtd);
+		printf("\n==========================================\n\n");
+		
+		codigo = valida_numero();
 	}
-	return 0;
-}/////////////////////
 
-float calculaPreco20(int cod, float preco){
+	return 0;
+}
+//*** FIM DO BLOCO PRINCIPAL **********************************************
+
+float preco_20_porcento(float preco){
 	
-	printf("\nC�digo: %d Pre�o antigo: %.2f",cod,preco);
 	preco += (preco * 20)/100;
-	printf("\nC�digo: %d Pre�o novo: %.2f\n",cod,preco);
 	
 	return preco;
 }
 
+int valida_numero(){
+	int num;
+	
+	do{
+		printf("Digite o numero do codigo: ");
+		scanf("%d",&num);
+		if(num < 0){
+			printf("\nNumero negativo. Programa finalizado!\n\n");
+			return -1;
+		}
+	}while(num < 0);
+	
+	return num;
+}
