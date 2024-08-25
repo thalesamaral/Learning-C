a7b6a55e35725fea774de7f7ee0b19e68a490f13
Thales <thaleslima225@gmail.com>
2024-08-24 23:33:25 -0300

    A done

--- Lista_Exercicio_C/Lista_Exercicio_C_05-Funcao/lista05_ex09-Conta_Banco_01.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_05-Funcao/lista05_ex09-Conta_Banco_01.c
@@ -6,49 +6,83 @@ a) Receba do usuário o valor do saldo de uma conta bancária,
 uma operação bancária (débito ou crédito) e o valor desta operação,
 e utilize um subprograma para calcular e imprimir o valor do saldo após a operação;
 */
+// Entrada..: valor do saldo; operação (débito ou crédito); valor desta operação
+// Saída....: calcular e imprimir o valor do saldo após a operação
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
 
 //Protótipo de Função
-calcularSaldo(float, char, float);
+float loop_valor_positivo();
+void valida_resposta(char*);
 
+//*** BLOCO PRINCIPAL *****************************************************
 int main(void){
 //Declarações
-	float saldo, valorOp;
+	float saldo, valorOperacao;
 	char operacao;
 	
 //Instruções
 	
-	do{
-		printf("Saldo da conta bancaria: ");
-		scanf("%f",&saldo);
-	}while(saldo<0);
+	printf("Saldo da conta bancaria\n");
+	saldo = loop_valor_positivo();
 	
-	do{
-		printf("\nOperacao bancaria [C]Credito ou [D]Debito: ");
-		operacao = getche();
-		operacao = toupper(operacao);
-	}while((operacao != 'C') && (operacao != 'D'));
+	printf("\nOperacao bancaria [C]Credito ou [D]Debito\n");
+	valida_resposta(&operacao);
+
+	printf("\nValor da operacao\n");
+	valorOperacao = loop_valor_positivo();
 	
-	do{
-		printf("-1 para finalizar!\n");
-		printf("\nValor da operacao: ");
-		scanf("%f",&valorOp);
-	}while(valorOp<0);
-	
-	calcularSaldo(saldo, operacao, valorOp);
+	//calcularSaldo(saldo, operacao, valorOp);
+	if(operacao == 'C'){
+		saldo += valorOperacao;
+	}else if(operacao == 'D'){
+		saldo -= valorOperacao;
+	}
+
+	printf("\nSaldo final: %.2f",saldo);
 	
 	return 0;
 }
+//*** FIM DO BLOCO PRINCIPAL **********************************************
 
+float loop_valor_positivo(){
+	float valor;
+	
+	do{
+		printf("Insira o valor: ");
+		scanf("%f",&valor);
+		if(valor <= 0){
+			printf("\nValor invalido, apenas numeros positivos\n\n");
+		}
+	}while(valor <= 0);
+	
+	return valor;
+}
+
+void valida_resposta(char *resposta){
+	
+	do{
+		fflush(stdin);
+		printf("Resposta: ");
+		*resposta = getchar();
+		while (getchar() != '\n');
+		*resposta = toupper(*resposta);
+		if((*resposta != 'C') && (*resposta != 'D')){
+			printf("\nResposta invalida!\n\n");
+		}
+	}while((*resposta != 'C') && (*resposta != 'D'));
+}
+
+/*
 calcularSaldo(float saldo, char op, float valor){
 	
-	if(op == 'C')
+	if(op == 'C'){
 		saldo += valor;
-	else
+	}else if(op == 'C'){
 		saldo -= valor;
+	}
 
 	printf("\nSaldo atual: %.2f",saldo);
 }
-
-
+*/
