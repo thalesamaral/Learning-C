d3aca3fe2b082acaf6dedb5e19bf7dbbbb73a9fe
Thales <thaleslima225@gmail.com>
2024-08-26 18:50:03 -0300

    done

--- Lista_Exercicio_C/Lista_Exercicio_C_06-Vetor/lista06_ex01-Vetor_Inverso.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_06-Vetor/lista06_ex01-Vetor_Inverso.c
@@ -1,10 +1,12 @@
 //  Síntese
 //  Nome....: "Thales Amaral Lima"
 //  Data....: "14/12/2021"
-/*	Objetivo: leia os elementos de um vetor de 20 posi��es de reais (float) e apresente o 
-vetor na ordem inversa � leitura.*/
+/*	Objetivo:
+1. Faça um programa em C que leia os elementos de um vetor de 20 posições de reais (float) 
+e apresente o vetor na ordem inversa à leitura.
+*/
 //  Entrada.: vetor de float.
-//  Sa�da...: imprimir a sequ�ncia inversa.
+//  Saída...: imprimir a sequencia inversa.
 #include<stdio.h>
 #include<stdlib.h>
 #define TAM 3
@@ -15,17 +17,16 @@ int main(void){
 	float valor[TAM];
 	int i;
 	
-//Instruções
-	//printf("");
-	//scanf("%",&);
-	
+//Instruções	
 	for(i=0; i<TAM; i++){
-		printf("%d� valor: ",i+1);
+		printf("%do Valor: ",i+1);
 		scanf("%f",&valor[i]);
 	}
 	
+	printf("\nInverso\n");
+
 	for(i=TAM-1; i>=0; i--){
-		printf("\n%d� valor: %.2f",i+1,valor[i]);
+		printf("%do Valor = %.2f\n",i+1,valor[i]);
 	}
 	
 	return 0;
