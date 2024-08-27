8f3353f54c32f171a284b80024f03cb9353c031b
Thales <thaleslima225@gmail.com>
2024-08-27 19:13:56 -0300

    4 done

--- Lista_Exercicio_C/Lista_Exercicio_C_06-Vetor/lista06_ex04-Troca_Numeros.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_06-Vetor/lista06_ex04-Troca_Numeros.c
@@ -1,11 +1,14 @@
 //  Síntese
 //  Nome....: "Thales Amaral Lima"
 //  Data....: "14/12/2021"
-/*	Objetivo: leia os elementos de um vetor de 20 posi��es de inteiros e apresente o vetor. 
-Em seguida troque o primeiro elemento com o �ltimo, o segundo com o pen�ltimo, o terceiro com o 
-antepen�ltimo, e assim sucessivamente. Por fim, apresente novamente o vetor depois das trocas.*/
-//  Entrada.: Um vetor de int.
-//  Sa�da...: vetor valores invertidos
+/*	Objetivo:
+4. Faça um programa em C que leia os elementos de um vetor de 20 posições de inteiros e 
+apresente o vetor. Em seguida troque o primeiro elemento com o último, o segundo com o 
+penúltimo, o terceiro com o antepenúltimo, e assim sucessivamente. Por fim, apresente 
+novamente o vetor depois das trocas.
+*/
+//  Entrada.: vetor de int
+//  Saida...: vetor invertido
 #include<stdio.h>
 #include<stdlib.h>
 #define TAM 5
@@ -16,28 +19,35 @@ int main(void){
 	int i, j, v[TAM], aux;
 	
 //Instruções
-	//printf("");
-	//scanf("%",&);
-	
+	/*
 	for(i=0; i<TAM; i++){
-		printf("%d� valor: ",i+1);
+		printf("%do Valor: ",i+1);
 		scanf("%d",&v[i]);
 	}
-	
-	printf("\nAntes\n");
+	*/
+
+	v[0] = 0;
+	v[1] = 1;
+	v[2] = 2;
+	v[3] = 3;
+	v[4] = 4;
+
+	printf("\nVetor Original\n");
 	for(i=0; i<TAM; i++){
-		printf("%d� valor: %d\n",i,v[i]);
+		printf("%do Valor: %d\n",i+1,v[i]);
 	}
-	
+	/*
+	i = primeiro & i++; j = ultimo & j--
+	*/
 	for(i=0, j=TAM-1; i<TAM/2; i++, j--){
 		aux = v[i];
 		v[i] = v[j];
 		v[j] = aux;
 	}
 	
-	printf("\nDepois");
+	printf("\nVetor Invertido");
 	for(i=0; i<TAM; i++){
-		printf("\n%d� valor: %d",i,v[i]);
+		printf("\n%do Valor: %d",i+1,v[i]);
 	}
 	
 	return 0;
