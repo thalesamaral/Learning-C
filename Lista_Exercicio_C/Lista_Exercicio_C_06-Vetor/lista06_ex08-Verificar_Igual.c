f206a75e2d615ece47ffdc53d47cdb69e2aeb8a8
Thales <thaleslima225@gmail.com>
2024-08-27 20:07:27 -0300

    update 8

--- Lista_Exercicio_C/Lista_Exercicio_C_06-Vetor/lista06_ex08-Verificar_Igual.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_06-Vetor/lista06_ex08-Verificar_Igual.c
@@ -1,45 +1,43 @@
 //  Síntese
 //  Nome....: "Thales Amaral Lima"
 //  Data....: "15/12/2021"
-/*	Objetivo: leia um vetor de 20 posi��es, verifique se existem valores iguais dentro dele e 
-os escreva na tela*/
+/*	Objetivo:
+8. Faça um programa em C que leia um vetor de 20 posições, verifique se existem valores 
+iguais dentro dele e os escreva na tela
+*/
 //  Entrada.: um vetor de int.
-//  Sa�da...: imprime valores iguais.
+//  Saida...: imprime valores iguais.
 #include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
 #define TAM 5
 
 //*** BLOCO PRINCIPAL *****************************************************
 int main(void){
 //Declarações
-	int i=0, j=0, vet[TAM], flag=0;
+	int i=0, j=0, vet[TAM], flag=0, valorIgual;
 	
-//Instruções
-	//printf("");
-	//scanf("%",&);
-	
+//Instruções	
+	printf("\nVetor\n");
 	for(i=0; i<TAM; i++){
-		printf("Vetor - %d� valor: ",i+1);
+		printf("%do Valor: ",i+1);
 		scanf("%d",&vet[i]);
 	}
 	
 	for(i=0; i<TAM; i++){
 		for(j=i+1; j<TAM; j++){
-			if(vet[i]==vet[j]){
-				printf("\n%d� valor: %d",i+1, vet[i]);
-				printf("\n%d� valor: %d",j+1, vet[j]);
+			if(vet[i] == vet[j]){
+				//valorIgual = vet[i];
+				printf("\n%do Valor: %d",i+1, vet[i]);
+				printf("\n%do Valor: %d\n",j+1, vet[j]);
 				flag=1;
+				break;
 			}
 		}
 	}
 	
-	if(!flag)
-		printf("\nN�o h� repetidos!");
+	if(!flag){
+		printf("\nNao tem repetidos!\n");
+	}
 		
 	return 0;
 }
 //*** FIM DO BLOCO PRINCIPAL **********************************************
-
-
-
