64bfb63b2fc5a098dbe724d924d6563a051e9cb3
Thales <thaleslima225@gmail.com>
2024-09-01 03:31:55 -0300

    9-10 done

--- Lista_Exercicio_C/Lista_Exercicio_C_07-String/lista07_ex10-Palindromo.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_07-String/lista07_ex10-Palindromo.c
@@ -1,53 +1,63 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
 //  Data....: "19/12/2021"
-/*	Objetivo: receba um texto e informe se � ou n�o pal�ndromo. Despreze espa�os em branco e considere que n�o 
-ser� utilizado pontua��o ou acentua��o. Exemplos: aba, radar, reter, rever, rir, rotor, ovo, arara, �anotaram a 
-data da maratona�.*/
-//  Entrada.:
-//  Sa�da...:
+/*	Objetivo:
+10.Palíndromo é uma palavra, frase ou qualquer outra sequência de unidades (como uma 
+cadeia de DNA; Enzima de restrição) que tenha a propriedade de poder ser lida tanto da 
+direita para a esquerda como da esquerda para a direita. Num palíndromo, normalmente são 
+desconsiderados os sinais ortográficos (diacríticos ou de pontuação) 
+(http://pt.wikipedia.org/wiki/Palíndromo). Faça um programa em C que receba um texto e 
+informe se é ou não palíndromo. Despreze espaços em branco e considere que não será 
+utilizado pontuação ou acentuação. Exemplos: aba, radar, reter, rever, rir, rotor, ovo, arara, 
+“anotaram a data da maratona”
+*/
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#define TAM 30
+#define TAM 100
 
-//Prototipo de funcoes
+//Protótipo de função
 int palindromo(char[]);
 
 //*** BLOCO PRINCIPAL *****************************************************
 int main(void){
 //Declarações
-	char text[TAM];
+	char texto[] = "anotaram a data da maratona";
 
 //Instruções
+	/*
 	printf("Digite um texto: ");
-	gets(text);
+	fgets(texto, TAM, stdin);
+	texto[strlen(texto)-1] = '\0'; //Ignora as casas não utilizadas
+	*/
 	
-	if(palindromo(text))
-		printf("\nPal�ndromo");
-	else
-		printf("\nN�o Pal�ndromo");
+	if(palindromo(texto)){
+		printf("\nPalindromo\n");
+	}else{
+		printf("\nNao Palindromo\n");
+	}
 	
 	return 0;
 }
 
 int palindromo(char str[]){
 	int ini, fim=strlen(str)-1;
-	
+
 	while(ini < fim){
-		if(str[ini] == ' ')
+		if(str[ini] == ' '){
 			ini++;
-		else{
+		}else{
 			if(str[fim] == ' ')
 				fim--;
 			else{
-				if(str[ini] != str[fim])
+				if(str[ini] != str[fim]){
 					return 0;
+				}
 				ini++;
 				fim--;
 			}
 		}
 	}
+	
 	return 1;
 }
 //*** FIM DO BLOCO PRINCIPAL **********************************************
