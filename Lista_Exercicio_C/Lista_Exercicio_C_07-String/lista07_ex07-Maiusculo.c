1e24cf306c71fdd7a522c153af378e5a8fcfeadf
Thales <thaleslima225@gmail.com>
2024-09-01 01:31:23 -0300

    7 done

--- Lista_Exercicio_C/Lista_Exercicio_C_07-String/lista07_ex07-Maiusculo.c
+++ Lista_Exercicio_C/Lista_Exercicio_C_07-String/lista07_ex07-Maiusculo.c
@@ -1,27 +1,30 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "19/12/2021"
-/*	Objetivo: */
-//  Entrada.:
-//  Sa�da...:
+//  Data....: "01/09/2024"
+/*	Objetivo:
+7. Faça um programa em C que receba um texto, converta todos os caracteres deste para 
+maiúsculo e apresente o texto após convertido. Observação: o código ASCII da letra ‘a’ 
+(minúsculo) é 97 e o código da letra ‘A’ (maiúsculo) é 65, o código da letra ‘z’ é 122 e o código 
+da letra ‘Z’ é 90. Não utilize nenhuma função além das vistas na aula.
+*/
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#define TAM 50
+#define TAM 255
 
 void maiusculo(char[]);
 
 //*** BLOCO PRINCIPAL *****************************************************
 int main(void){
 //Declarações
-	char text[TAM];
+	char texto[TAM];
 
 //Instruções
 	printf("Digite um texto: ");
-	gets(text);
+	fgets(texto, TAM, stdin);
+	texto[strlen(texto)-1] = '\0'; //Ignora as casas não utilizadas
 	
-	maiusculo(text);
-	printf("Texto em maiusculo: %s",text);
+	maiusculo(texto);
+	printf("\nTexto em maiusculo: %s\n",texto);
 	
 	return 0;
 }
@@ -29,8 +32,28 @@ int main(void){
 void maiusculo(char str[]){
 	int i;
 	
-	for(i=0; i<strlen(str); i++)
-		if((str[i] >= 'a') && (str[i] <= 'z'))
+	for(i=0; i<strlen(str); i++){
+		if((str[i] >= 'a') && (str[i] <= 'z')){
 			str[i] -= 32;
+		}
+	}
 }
 //*** FIM DO BLOCO PRINCIPAL **********************************************
+/*
+No código C, a variável texto[] é passada para a função maiusculo() como um argumento, e qualquer modificação feita na variável dentro da função maiusculo() é refletida no bloco principal (main). Isso ocorre porque, em C, arrays são passados para funções como ponteiros.
+
+Quando você passa um array para uma função, o que é realmente passado é o endereço de memória do primeiro elemento do array. Isso significa que a função maiusculo() não recebe uma cópia do array, mas sim uma referência ao array original. Assim, quando você altera o conteúdo do array dentro da função, você está alterando diretamente o conteúdo da variável original, que é acessada por referência.
+
+Declaração da Função: A função maiusculo(char str[]) recebe um array de caracteres como argumento. Na prática, char str[] é equivalente a char *str, ou seja, um ponteiro para o primeiro elemento do array.
+
+Esse comportamento é típico em C, onde a passagem de arrays para funções é feita por referência, ao contrário da passagem de variáveis escalares (como int ou float), que é feita por valor (cópia).
+*/
+
+/* OUTRA SOLUÇÃO
+	for(i=0; i<strlen(texto); i++){
+		if(texto[i] >= 97 && texto[i] <= 122){
+			posiMinusc = texto[i] - 96;
+			texto[i] = 64 + posiMinusc;
+		}
+	}
+*/
